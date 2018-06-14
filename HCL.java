5cc4bfb7beca7c1b0d01be59fa2718f438df9409
chetan0209 <chetanbhandari0209@gmail.com>
2018-06-14 22:47:12 +0530

    Create HCL.java

---
+++ HCL.java
@@ -0,0 +1,23 @@
+IOC ?
+Spring MVC Architecture ?
+How to improve Project Performence Write Steps ?
+Answer: 
+
+How to improve Database performence?
+How to write indexing in db?
+How to implement console  HQL in hibernate?
+How to Implement Cache ?
+How to decide to use LinkedHashMap or HashMap ?
+What is diff b/w HashSet and TreeSet , Which is peroformence is high?
+
+
+
+Hibernate :
+How to implement cache?
+How to implement one to one or one to many mapping in hibernate.
+How to implement connection polling in hibernate ? using cp30 or p30
+How to show hql on console ?
+What is diff b/w load and get method ?
+what is the life cycle of hibernate object state ?
+What are annotation used in hibernate ?
+How to configure hibernate ?
